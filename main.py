68c0b06d6b698ae5e2cc3bef3ef36724eaf909dc
tnsk25 <saikrishnantn@gmail.com>
2019-02-17 05:01:53 -0600

    Added MS-Candidate Gen function and other minor changes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import copy
 import operator
 from collections import OrderedDict
 
@@ -47,6 +48,7 @@ def MsGsp(S, MIS, SDC):
 	CountMap = {}
 	LMap = {}
 	F1 = []
+	F = {}
 
 	for i in sorted(MIS, key=MIS.get, reverse=False) :
 		M.append(i)
@@ -78,6 +80,8 @@ def MsGsp(S, MIS, SDC):
 			 F1.append(L[i][0])
 
 	#print("F1\n", F1)
+
+	MScandidateGen(F,M,CountMap,SDC,MIS)
 
 
 def init_pass(M,CountMap,seqCount,MIS,LMap):
@@ -118,6 +122,146 @@ def level_2():
 					C2.append([[L[j][0]], [L[i][0]]])
 	return C2
 
+
+def MScandidateGen(F,M,CountMap,SDC,MIS):
+	F[2] = [[[20, 30]], [[20], [30]], [[20, 70]], [[20], [70]], [[20], [80]], [[30], [30]], [[30, 70]], [[30], [70]], [[30, 80]], [[30], [80]], [[70], [70]], [[70, 80]], [[80], [70]], [[10, 40]], [[10], [40]], [[40], [40]]]
+	# F[2] = [[[20, 30, 40]], [[40], [70]]]
+	C = []
+	for i in range(len(F[2])):
+		for j in range(len(F[2])):
+			s1 = F[2][i]
+			s2 = F[2][j]
+			first_s1 = getFirstItem(s1)
+			last_s1 =  getLastItem(s1)
+			first_s2 = getFirstItem(s2)
+			last_s2 =  getLastItem(s2)
+			MIS_least_seq = getMISofSequence(s1,MIS)
+			
+			if( MIS[first_s1] < MIS_least_seq ):
+				if( (removeItem(s1,1) == removeItem(s2,Length(s2)-1)) & (MIS[last_s2] > MIS[first_s1]) ):
+					if(Size(LastElement(s2)) == 1):	
+						c1 = []		
+						c1 = s1
+						c1.append(LastElement(s2))
+						C.append(c1)
+
+						if( (Length(s1) == 2 & Size(s1) == 2) & (getLastItem(s2) > getLastItem(s1)) ):
+							c2 = []
+							c2 = s1
+							last_c2 = LastElement(c2)
+							last_c2.extend(getLastItem(s2))
+							# removeItem(c2,Length(c2)-1)
+							C.append(c2)
+					elif( ((Length(s1) == 2 & Size(s1) == 1) & (getLastItem(s2) > getLastItem(s1))) | ( Length(s1) > 2 ) ):
+						c2 = []
+						c2 = s1	
+						last_item_s2 = getLastItem(s2)	
+						last_c2 = LastElement(c2)
+						last_c2.append(last_item_s2)
+						C.append(c2)
+
+			elif( MIS[getLastItem(s2)] <  getMISofSequence(s2,MIS) ):	
+				if( (removeItem(s2,1) == removeItem(s1,Length(s1)-1)) & (MIS[first_s1] > MIS[last_s2]) ):
+					if(Size(FirstElement(s1)) == 1):	
+						c1 = []		
+						c1 = s2
+						c1.append(FirstElement(s1))
+						C.append(c1)
+							
+						if( (Length(s2) == 2 & Size(s2) == 2) & (getFirstItem(s1) > getFirstItem(s2)) ):
+							c2 = []
+							c2 = s2
+							last_c2 = FirstElement(c2)
+							last_c2.extend(getFirstItem(s1))
+							# removeItem(c2,0)
+							C.append(c2)
+					elif( ((Length(s2) == 2 & Size(s2) == 1) & (getFirstItem(s1) > getFirstItem(s2))) | ( Length(s2) > 2 ) ):
+						c1 = []
+						c1 = s2	
+						last_item_s1 = getFirstItem(s1)	
+						last_c1 = FirstElement(c1)
+						last_c1.append(last_item_s1)
+						C.append(c1)
+
+			else:					
+				if(removeItem(s1,0) == removeItem(s2,Length(s2)-1)):
+					if(Size(LastElement(s2)) == 1):	
+						c1 = []
+						c1 = s1
+						c1.append(LastElement(s2))
+						C.append(c1)
+					if(Size(LastElement(s2)) > 1):
+						c1 = []
+						c1 = s1	
+						last_item_s2 = getLastItem(s2)	
+						last_c1 = LastElement(c1)
+						last_c1.extend(last_item_s2)
+						# removeItem(c1,Length(c1)-1)
+						C.append(c1)
+
+	return C					
+
+
+def getFirstItem(s):
+	first = 0
+	for i in s:
+		first =i[0]
+		break
+	return first
+
+def getLastItem(s):
+	last = 0
+	for i in s:
+		last = i[-1]
+	return last	
+
+
+def removeItem(s,index):
+
+		seqnew = copy.deepcopy(s)
+		length = Length(s)
+		if index < 0 or index >= length:
+			return []
+		count = 0
+		for element in seqnew:
+			if count + len(element) <= index:
+				count += len(element)
+			else:
+				del element[index - count]
+				break
+		return [element for element in seqnew if len(element) > 0]
+
+
+def getMISofSequence(s,MIS):
+	temp = []
+	MIS_array = []
+	for i in s:
+		for j in range(len(i)):
+			temp.append(i[j])
+
+	for elem in temp:
+		MIS_array.append(MIS[elem])
+
+	return min(MIS_array)	
+
+
+def Size(s):
+   return len(s)
+
+
+def Length(s):
+   l = 0
+   for i in s:
+       l += len(i)
+   return l
+
+def LastElement(s):
+   last = s[-1]
+   return last
+
+def FirstElement(s):
+   first = s[0]
+   return first   
 
 def BetweenBracket(text, a, b):
 	pos_a = text.find(a)
